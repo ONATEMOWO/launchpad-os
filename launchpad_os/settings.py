08a4659ec68adc61a465aa0b84ac974382b00606
ONATEMOWO <folutek@gmail.com>
2026-04-23 20:54:37 -0500

    Disable debug toolbar in production

--- launchpad_os/settings.py
+++ launchpad_os/settings.py
@@ -17,7 +17,7 @@ SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
 SECRET_KEY = env.str("SECRET_KEY")
 SEND_FILE_MAX_AGE_DEFAULT = env.int("SEND_FILE_MAX_AGE_DEFAULT")
 BCRYPT_LOG_ROUNDS = env.int("BCRYPT_LOG_ROUNDS", default=13)
-DEBUG_TB_ENABLED = DEBUG
+DEBUG_TB_ENABLED = DEBUG  # toolbar only active when DEBUG is True; never in production
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 CACHE_TYPE = (
     "flask_caching.backends.SimpleCache"  # Can be "MemcachedCache", "RedisCache", etc.
